511679e615aa10ba0142d42a734ae03ba9ae2ebb
Muneeb Malik <abrarulhaqfarooui68@gmail.com>
2024-09-23 21:33:04 +0500

    'checking'

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
 
+
 export const Login = () => {
+
+  const [formData, setFormData]  = useState({
+    email:"",
+    password:""
+  })
+  const changeHandler = (e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const userlogin = async ()=>{
+    console.log(formData);
+    let responseData;
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }else{
+      alert(responseData.error)
+    }
+  }
+  
   return (
    
     <div className='login'>
         <div className='login-container'>
           <h1>Login</h1>
           <div className='login-fields'>
-            <input  type='email' placeholder='Email-Address'  />
-            <input  type='password' placeholder='Password'  />
+            <input name='email' value={formData.email} onChange={changeHandler} type='email' placeholder='Email-Address'  />
+            <input name='password' value={formData.password} onChange={changeHandler} type='password' placeholder='Password'  />
              </div>   
-             <button>Continue</button>      
+             <button onClick={()=>{userlogin()}}>Continue</button>      
              <p className='login-login'>Don't have an account? <Link  style={{textDecoration: 'none', color: 'Red'}}  to="/Signup"    >Signup Here</Link>  </p>
              <div  className='login-agree'>
               <input  type='checkbox'   name=''  id=''  />
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './Signup.css';
 export const Signup = () => {
+
+  const [formData, setFormData]  = useState({
+    username:"",
+    email:"",
+    password:""
+  })
+  const changeHandler = (e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const usersignup = async ()=>{
+    console.log(formData);
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+    if(responseData.success){
+      localStorage.setItem('auth-token', responseData.token);
+      window.location.replace("/");
+    }else{
+      alert(responseData.error)
+    }
+  }
+
   return (
    
 
@@ -9,11 +38,11 @@ export const Signup = () => {
     <div className='signup-container'>
       <h1>Signup</h1>
       <div className='signup-fields'>
-        <input  type='text' placeholder='Your Name'  />
-        <input  type='email' placeholder='Email-Address'  />
-        <input  type='password' placeholder='Password'  />
+        <input name='username' value={formData.username} onChange={changeHandler} type='text' placeholder='Your Name'  />
+        <input name='email' value={formData.email} onChange={changeHandler} type='email' placeholder='Email-Address'  />
+        <input name='password' value={formData.password} onChange={changeHandler} type='password' placeholder='Password'  />
          </div>   
-         <button>Continue</button>      
+         <button onClick={()=>{usersignup()}}>Continue</button>      
          <p className='signup-signup'>Already have an account? <Link  style={{textDecoration: 'none', color: 'Red'}}  to="/Login"    >Login Here</Link>  </p>
          <div  className='signup-agree'>
           <input  type='checkbox'   name=''  id=''  />
